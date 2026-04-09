900942a90cfa1d22fb7eb429cbef6fe9b1bae08e
Jose Benito Monsanto <josebenitomonsanto@gmail.com>
2026-04-09 21:41:51 +0800

    the html, css, and js are rephrased to improve the interface of the website

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const text = "Aspiring Junior Developer";
+let i = 0;
+
+function typeEffect() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeEffect, 100);
+  }
+}
+
+typeEffect();
+
 function scrollToProjects() {
   document.getElementById("projects").scrollIntoView({
     behavior: "smooth"
